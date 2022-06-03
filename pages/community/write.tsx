b43fb29dbbd5d9e94b9c77e6305d4da0878178f1
ing928 <kik928@nate.com>
2022-06-03 15:33:40 +0900

    Make form & api router

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -1,14 +1,45 @@
+import { Post } from "@prisma/client";
+import useMutation from "libs/client/useMutation";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 import Button from "../../components/button";
 import Layout from "../../components/layout";
 import TextArea from "../../components/textarea";
 
+//Form에 들어오는 type
+interface IForm {
+  question: string;
+}
+//useMutation의 반환값 type
+interface IPost {
+  ok: boolean;
+  post: Post;
+}
+
 const Write: NextPage = () => {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<IForm>();
+  const [post, { loading, data }] = useMutation<IPost>("/api/posts");
+  const onValid = (data: IForm) => {
+    if (loading) return;
+    post(data);
+  };
+  useEffect(() => {
+    if (data && data.ok) {
+      router.push(`/community/${data.post.id}`);
+    }
+  }, [data, router]);
   return (
     <Layout canGoBack title="Write Post">
-      <form className="p-4 space-y-4">
-        <TextArea required placeholder="Ask Question!" />
-        <Button text="Save" />
+      <form onSubmit={handleSubmit(onValid)} className="p-4 space-y-4">
+        <TextArea
+          register={register("question", { required: true })}
+          required
+          placeholder="Ask Question!"
+        />
+        <Button text={loading ? "Loading..." : "Save"} />
       </form>
     </Layout>
   );
