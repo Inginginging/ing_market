2d9722ca060b4aea187a1dfa09268bfafbd5d182
ing928 <kik928@nate.com>
2022-06-07 19:56:15 +0900

    favs, sales, purchases api url

--- pages/api/users/me/favs.ts
+++ pages/api/users/me/favs.ts
@@ -1,19 +1,26 @@
 import { withApiSession } from "libs/server/withSession";
 import { NextApiRequest, NextApiResponse } from "next";
-import client from "../../../libs/server/client";
-import withHandler, { ResponseType } from "../../../libs/server/withHandler";
+import client from "../../../../libs/server/client";
+import withHandler, { ResponseType } from "../../../../libs/server/withHandler";
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  //session에 저장된 user의 id와 동일한 id를 가진 user를 db에서 찾아와 할당
-  const profile = await client.user.findUnique({
-    where: { id: req.session.user?.id },
+  const {
+    session: { user },
+  } = req;
+  const favs = await client.fav.findMany({
+    where: {
+      userId: user?.id,
+    },
+    include: {
+      product: true,
+    },
   });
   return res.json({
     ok: true,
-    profile,
+    favs,
   });
 }
 
---
+++ pages/api/users/me/index.ts
@@ -0,0 +1,20 @@
+import { withApiSession } from "libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "../../../../libs/server/client";
+import withHandler, { ResponseType } from "../../../../libs/server/withHandler";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  //session에 저장된 user의 id와 동일한 id를 가진 user를 db에서 찾아와 할당
+  const profile = await client.user.findUnique({
+    where: { id: req.session.user?.id },
+  });
+  return res.json({
+    ok: true,
+    profile,
+  });
+}
+
+export default withApiSession(withHandler({ methods: ["GET"], handler }));
