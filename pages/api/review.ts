90ae3f5bf5fbc5d0c5a70f99befca77729307a9b
ing928 <kik928@nate.com>
2022-06-07 19:41:11 +0900

    /api/review

---
+++ pages/api/review.ts
@@ -0,0 +1,31 @@
+import { withApiSession } from "libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "../../libs/server/client";
+import withHandler, { ResponseType } from "../../libs/server/withHandler";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { user } = req.session;
+  const reviews = await client.review.findMany({
+    where: {
+      createdForId: user?.id,
+    },
+    include: {
+      createdBy: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  return res.json({
+    ok: true,
+    reviews,
+  });
+}
+
+export default withApiSession(withHandler({ methods: ["GET"], handler }));
