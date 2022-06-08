ab2c27d65a560d8ef59fe8271173085fb3486374
ing928 <kik928@nate.com>
2022-06-08 21:08:13 +0900

    Stream model & create form

--- pages/streams/create.tsx
+++ pages/streams/create.tsx
@@ -1,15 +1,50 @@
+import { Stream } from "@prisma/client";
+import useMutation from "libs/client/useMutation";
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 import Button from "../../components/button";
 import Input from "../../components/input";
 import Layout from "../../components/layout";
 import TextArea from "../../components/textarea";
 
+interface IFormData {
+  name: string;
+  description: string;
+  price: string;
+}
+interface IApiResponse {
+  ok: boolean;
+  stream: Stream;
+}
+
 const Create: NextPage = () => {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<IFormData>();
+  const [createStream, { loading, data }] =
+    useMutation<IApiResponse>(`api/streams`);
+  const onValid = (form: IFormData) => {
+    if (loading) return;
+    createStream(form);
+  };
+  useEffect(() => {
+    if (data && data.ok) {
+      router.push(`/streams/${data.stream.id}`);
+    }
+  }, [data, router]);
   return (
     <Layout canGoBack title="Create Live">
-      <form className=" space-y-4 py-10 px-4">
-        <Input required label="Name" name="name" type="text" />
+      <form onSubmit={handleSubmit(onValid)} className=" space-y-4 py-10 px-4">
         <Input
+          register={register("name", { required: true })}
+          required
+          label="Name"
+          name="name"
+          type="text"
+        />
+        <Input
+          register={register("price", { required: true })}
           required
           label="Price"
           placeholder="0.00"
@@ -17,8 +52,12 @@ const Create: NextPage = () => {
           type="text"
           kind="price"
         />
-        <TextArea name="description" label="Description" />
-        <Button text="Start Live" />
+        <TextArea
+          register={register("description", { required: true })}
+          name="description"
+          label="Description"
+        />
+        <Button text={loading ? "Loading..." : "Start Live"} />
       </form>
     </Layout>
   );
