4e02f81c7bdd28a80c5cac7aaecba0c23e213f1c
ing928 <kik928@nate.com>
2022-06-14 17:32:00 +0900

    avatar preview

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -1,7 +1,7 @@
 import useMutation from "libs/client/useMutation";
 import useUser from "libs/client/useUser";
 import type { NextPage } from "next";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import Button from "../../components/button";
 import Input from "../../components/input";
@@ -11,6 +11,7 @@ interface IEditForm {
   email?: string;
   phone?: string;
   name?: string;
+  avatar?: FileList;
   formErrors?: string;
 }
 
@@ -27,6 +28,7 @@ const EditProfile: NextPage = () => {
     setValue,
     setError,
     formState: { errors },
+    watch,
   } = useForm<IEditForm>();
   const [editProfile, { data, loading }] =
     useMutation<IEditProfileResponse>(`/api/users/me`);
@@ -35,7 +37,7 @@ const EditProfile: NextPage = () => {
     if (user?.phone) setValue("phone", user.phone);
     if (user?.name) setValue("name", user.name);
   }, [user]);
-  const onValid = ({ email, phone, name }: IEditForm) => {
+  const onValid = ({ email, phone, name, avatar }: IEditForm) => {
     if (loading) return;
     if (email === "" && phone === "") {
       setError("formErrors", {
@@ -49,17 +51,34 @@ const EditProfile: NextPage = () => {
       setError("formErrors", { message: data.error });
     }
   }, [data, setError]);
+  const [avatarPreview, setAvatarPreview] = useState("");
+  const avatarExist = watch("avatar");
+  useEffect(() => {
+    if (avatarExist && avatarExist.length > 0) {
+      const file = avatarExist[0];
+      setAvatarPreview(URL.createObjectURL(file));
+    }
+  }, [avatarExist]);
+
   return (
     <Layout canGoBack title="Edit Profile">
       <form onSubmit={handleSubmit(onValid)} className="py-5 px-4 space-y-4">
         <div className="flex items-center space-x-3">
-          <div className="w-14 h-14 rounded-full bg-slate-500" />
+          {avatarPreview ? (
+            <img
+              src={avatarPreview}
+              className="w-14 h-14 rounded-full bg-slate-500"
+            />
+          ) : (
+            <div className="w-14 h-14 rounded-full bg-slate-500" />
+          )}
           <label
             htmlFor="picture"
             className="cursor-pointer py-2 px-3 border hover:bg-gray-50 border-gray-300 rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 text-gray-700"
           >
             Change
             <input
+              {...register("avatar")}
               id="picture"
               type="file"
               className="hidden"
