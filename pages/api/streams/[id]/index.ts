598121d1230aa3eea60b45aa63f9393bd4eb70b6
ing928 <kik928@nate.com>
2022-06-17 21:31:34 +0900

    securing stream

--- pages/api/streams/[id]/index.ts
+++ pages/api/streams/[id]/index.ts
@@ -9,6 +9,7 @@ async function handler(
 ) {
   const {
     query: { id },
+    session: { user },
   } = req;
   const stream = await client.stream.findUnique({
     where: {
@@ -29,6 +30,12 @@ async function handler(
       },
     },
   });
+  const isOwner = stream?.userId === user?.id; //stream의 주인일떄
+  //stream 주인이 아닐때 보이는 정보.
+  if (stream && !isOwner) {
+    stream.cloudflareKey = "xxxxx";
+    stream.cloudflareUrl = "xxxxx";
+  }
   return res.json({
     ok: true,
     stream,
